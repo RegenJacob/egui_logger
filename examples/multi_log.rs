d60831670fb40493fa1f4fb198b92fdad4dd4b89
Nicolas Bratoveanu <nicolasbratoveanu@proton.me>
2024-01-15 18:17:45 +0200

    Change visibility of EguiLogger to public (#14)
    * change visibility of EguiLogger
    
    this makes it possible to use egui_logger with multi_log
    
    * add multi_log example

---
+++ examples/multi_log.rs
@@ -0,0 +1,44 @@
+use eframe::NativeOptions;
+
+fn main() {
+    // Create an EguiLogger; multi_log will take care of initialization.
+    let egui_logger = Box::new(egui_logger::EguiLogger);
+
+    // And add another one.
+    let env_logger = Box::new(env_logger::builder().default_format().build());
+
+    multi_log::MultiLogger::init(vec![egui_logger, env_logger], log::Level::Debug)
+        .expect("Error initializing multi_logger");
+
+    eframe::run_native(
+        "egui_logger",
+        NativeOptions::default(),
+        Box::new(|_cc| Box::new(MultiLogApp::default())),
+    )
+    .expect("Couldn't run eframe app");
+}
+
+#[derive(Default)]
+struct MultiLogApp;
+
+impl eframe::App for MultiLogApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if ui.button("This produces Debug Info").clicked() {
+                log::debug!("Very verbose Debug Info")
+            }
+            if ui.button("This produces an Info").clicked() {
+                log::info!("Some Info");
+            }
+            if ui.button("This produces an Error").clicked() {
+                log::error!("Error doing Something");
+            }
+            if ui.button("This produces a Warning").clicked() {
+                log::warn!("Warn about something")
+            }
+        });
+        egui::Window::new("Log").show(ctx, |ui| {
+            egui_logger::logger_ui(ui);
+        });
+    }
+}
